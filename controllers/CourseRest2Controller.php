4b85e5d43c06e1aef5e6e4abd448f57d81977739
compmaxxx <compmaxxx@gmail.com>
2015-03-16 03:52:45 +0700

    allow origin course-rest2

--- controllers/CourseRest2Controller.php
+++ controllers/CourseRest2Controller.php
@@ -14,9 +14,23 @@ use yii\rest\Controller;
 
 class CourseRest2Controller extends Controller{
 
+    public function behaviors(){
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age' => 86400,
+            ],
+        ];
+        return $behaviors;
+    }
     public function actionViewAll(){
         return Course::find()->where(['is_active' => Course::STATE_ACTIVE])->all();
     }
+
     public function actionView($id){
         return Course::findOne($id);
     }
